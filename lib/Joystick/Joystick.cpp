df5294a1b8d87eaacfa6fb6baa3f573c28297c1d
ItsDuska <89298953+ItsDuska@users.noreply.github.com>
2025-11-25 13:42:12 +0200

    change joysticks a bit

--- lib/Joystick/Joystick.cpp
+++ lib/Joystick/Joystick.cpp
@@ -1,11 +1,8 @@
 #include "Joystick.h"
-#include <Arduino.h>
 
-
-
-// 0-100 degree range for joystick.
-constexpr uint8_t JOYSTICK_BEGIN_VAL = 0;
-constexpr uint8_t JOYSTICK_END_VAL = 100;
+// -100-100 degree range for joystick.
+constexpr int8_t JOYSTICK_BEGIN_VAL = -100;
+constexpr int8_t JOYSTICK_END_VAL = 100;
 constexpr int8_t JOYSTICK_DEADZONE_VAL = 7;
 
 Joystick::Joystick(u8 pinA, u8 pinB, u8 pinC)
@@ -14,19 +11,18 @@ Joystick::Joystick(u8 pinA, u8 pinB, u8 pinC)
     this->pinB = pinB;
     this->pinC = pinC;
 
-    pinMode(pinA, INPUT);      
-    pinMode(pinB, INPUT);        
+    pinMode(pinA, INPUT);
+    pinMode(pinB, INPUT);
 
     pinMode(pinC, INPUT_PULLUP);
 }
 
-Vec2u16 Joystick::readPosition()
+void Joystick::readPosition()
 {
-    position.x = map(analogRead(pinA), 0, 1023, -100, 100);
-    position.y = map(analogRead(pinB), 0, 1023, -100, 100);
+    position.x = map(analogRead(pinA), 0, 1023, JOYSTICK_BEGIN_VAL, JOYSTICK_END_VAL);
+    position.y = map(analogRead(pinB), 0, 1023, -JOYSTICK_BEGIN_VAL, JOYSTICK_END_VAL);
 
-
-    if (abs(position.x) < JOYSTICK_DEADZONE_VAL) 
+    if (abs(position.x) < JOYSTICK_DEADZONE_VAL)
     {
         position.x = 0;
     }
@@ -34,6 +30,11 @@ Vec2u16 Joystick::readPosition()
     {
         position.y = 0;
     }
+    return position;
+}
+
+Vec2i16 Joystick::getPosition()
+{
     return position;
 }
 
@@ -45,10 +46,10 @@ bool Joystick::isJoystickDown()
 
 Joystick::Direction Joystick::convertPositionToDirection()
 {
-    int16_t dx = position.x; 
+    int16_t dx = position.x;
     int16_t dy = position.y;
 
-    if (abs(dx) < JOYSTICK_DEADZONE_VAL) 
+    if (abs(dx) < JOYSTICK_DEADZONE_VAL)
     {
         dx = 0;
     }
@@ -58,19 +59,27 @@ Joystick::Direction Joystick::convertPositionToDirection()
     }
 
     bool right = dx > 15;
-    bool left  = dx < -15;
-    bool up    = dy < -15;
-    bool down  = dy > 15;
+    bool left = dx < -15;
+    bool up = dy < -15;
+    bool down = dy > 15;
 
-    if (up && right) return Direction::UP_RIGHT;
-    if (up && left)  return Direction::LEFT_UP;
-    if (down && right) return Direction::RIGHT_DOWN;
-    if (down && left)  return Direction::DOWN_LEFT;
+    if (up && right)
+        return Direction::UP_RIGHT;
+    if (up && left)
+        return Direction::LEFT_UP;
+    if (down && right)
+        return Direction::RIGHT_DOWN;
+    if (down && left)
+        return Direction::DOWN_LEFT;
 
-    if (up) return Direction::UP;
-    if (down) return Direction::DOWN;
-    if (right) return Direction::RIGHT;
-    if (left) return Direction::LEFT;
+    if (up)
+        return Direction::UP;
+    if (down)
+        return Direction::DOWN;
+    if (right)
+        return Direction::RIGHT;
+    if (left)
+        return Direction::LEFT;
 
     return Direction::IDLE;
 }
--- lib/Joystick/Joystick.h
+++ lib/Joystick/Joystick.h
@@ -1,25 +1,22 @@
 #pragma once
 #include <Arduino.h>
 
-
 // TODO: muuta tän nimi vec2i16 kun saadaan joystick toimimaan.
-struct Vec2u16
+struct Vec2i16
 {
     int16_t x;
     int16_t y;
 };
 
-// Position 50, 50 is 0,0
 class Joystick
 {
 public:
     Joystick(uint8_t pinA, uint8_t pinB, uint8_t pinC);
 
-    // reads the position and returns it. Also saves it internally
-    Vec2u16 readPosition(void);
+    Vec2i16 getPosition(void);
+    void readPosition(void);
     bool isJoystickDown();
 
-    // util functions
     enum class Direction
     {
         UP,
@@ -41,5 +38,5 @@ private:
     uint8_t pinC;
 
     bool buttonDown;
-    Vec2u16 position;
+    Vec2i16 position;
 };

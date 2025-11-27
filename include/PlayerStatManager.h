7a8e8c3aa4649f502253923123184fcb44caedac
luimu64 <luimu@luimu.dev>
2025-11-27 17:31:55 +0200

    format

--- include/PlayerStatManager.h
+++ include/PlayerStatManager.h
@@ -4,24 +4,22 @@
 constexpr uint8_t MAX_PLAYER_COUNT = 5;
 constexpr uint8_t MAX_NAME_LEN = 5;
 
-struct PlayerStat
-{
-    char name[5];
-    uint16_t score;
+struct PlayerStat {
+  char name[5];
+  uint16_t score;
 };
 
-class PlayerStatManager
-{
+class PlayerStatManager {
 public:
-    PlayerStatManager(uint16_t eepromAddress = 0);
-    void add(const PlayerStat &newStat);
-    void save();
-    void read();
+  PlayerStatManager(uint16_t eepromAddress = 0);
+  void add(const PlayerStat &newStat);
+  void save();
+  void read();
 
 private:
-    bool uniquePlayer(const PlayerStat &newStat);
+  bool uniquePlayer(const PlayerStat &newStat);
 
 private:
-    PlayerStat stats[MAX_PLAYER_COUNT];
-    uint16_t baseAddress;
+  PlayerStat stats[MAX_PLAYER_COUNT];
+  uint16_t baseAddress;
 };

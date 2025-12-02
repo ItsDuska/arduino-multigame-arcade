f63f2486124bf90d4adb674031ffec4984336e26
luimu64 <luimu@luimu.dev>
2025-12-02 13:49:58 +0200

    timerone mockki

---
+++ lib/EmbeddedMock/include/MockTimerOne.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <stddef.h>
+#include <stdint.h>
+
+class MockTimerOne {
+public:
+  MockTimerOne();
+
+  // --- TimerOne API ---
+  void initialize(unsigned long microseconds = 1000000);
+  void setPeriod(unsigned long microseconds);
+  void attachInterrupt(void (*isr)());
+  void detachInterrupt();
+  void start();
+  void stop();
+  void restart();
+
+  // PWM komennot (ei implementoitu)
+  void pwm(uint8_t pin, uint16_t duty);
+  void setPwmDuty(uint8_t pin, uint16_t duty);
+  void disablePwm(uint8_t pin);
+
+private:
+  unsigned long _period;      // Mikrosekunteina
+  unsigned long _lastRunTime; // Milloin viimeksi ajettiin
+  void (*_callback)();        // Tallennettu funktio
+  bool _running;
+};
+
+extern MockTimerOne Timer1;
---
+++ lib/EmbeddedMock/src/MockTimerOne.cpp
@@ -0,0 +1,41 @@
+#include "MockTimerOne.h"
+#include "MockArduino.h" // Tarvitaan micros()
+
+// Globaali instanssi
+MockTimerOne Timer1;
+
+MockTimerOne::MockTimerOne()
+    : _period(1000000), _lastRunTime(0), _callback(NULL), _running(false) {}
+
+void MockTimerOne::initialize(unsigned long microseconds) {
+  _period = microseconds;
+  _lastRunTime = micros();
+  _running = true;
+  _callback = NULL;
+}
+
+void MockTimerOne::setPeriod(unsigned long microseconds) {
+  _period = microseconds;
+}
+
+void MockTimerOne::attachInterrupt(void (*isr)()) {
+  _callback = isr;
+  _lastRunTime = micros(); // Nollataan laskuri ettei laukea heti ryppäänä
+  _running = true;
+}
+
+void MockTimerOne::detachInterrupt() { _callback = NULL; }
+
+void MockTimerOne::start() {
+  _running = true;
+  _lastRunTime = micros();
+}
+
+void MockTimerOne::stop() { _running = false; }
+
+void MockTimerOne::restart() { start(); }
+
+// Stubit PWM:lle - eivät tee simulaattorissa mitään
+void MockTimerOne::pwm(uint8_t pin, uint16_t duty) {}
+void MockTimerOne::setPwmDuty(uint8_t pin, uint16_t duty) {}
+void MockTimerOne::disablePwm(uint8_t pin) {}
